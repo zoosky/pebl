1ef1f526f19c12297acefbd0a2cf0d4741b4457c
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-25 15:58:40 -0400

    Future Proof tailwind

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,66 +1,69 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
   theme: {
     extend: {
       spacing: {
-        '80': '20rem',
-        '108': '27rem',
+        80: "20rem",
+        108: "27rem",
       },
       borderWidth: {
-        '14': '14px',
-      }
+        14: "14px",
+      },
     },
     container: {
-      padding: '1rem'
+      padding: "1rem",
     },
     colors: {
       background: {
-        '1': 'var(--bg-brand-1)',
-        '2': 'var(--bg-brand-2)',
-        '3': 'var(--bg-brand-3)',
+        1: "var(--bg-brand-1)",
+        2: "var(--bg-brand-2)",
+        3: "var(--bg-brand-3)",
       },
       copy: {
-        base: 'var(--text-brand-1)',
-        accent: 'var(--text-brand-2)',
-        mute: 'var(--text-brand-3)',
+        base: "var(--text-brand-1)",
+        accent: "var(--text-brand-2)",
+        mute: "var(--text-brand-3)",
       },
       primary: {
-        '1': 'var(--brand-primary-1)',
-        '2': 'var(--brand-primary-2)',
-        '3': 'var(--brand-primary-3)',
-        '4': 'var(--brand-primary-4)',
+        1: "var(--brand-primary-1)",
+        2: "var(--brand-primary-2)",
+        3: "var(--brand-primary-3)",
+        4: "var(--brand-primary-4)",
       },
       secondary: {
-        '1': 'var(--brand-secondary-1)',
-        '2': 'var(--brand-secondary-2)',
-        '3': 'var(--brand-secondary-3)',
-        '4': 'var(--brand-secondary-4)',
-        '5': 'var(--brand-secondary-5)',
+        1: "var(--brand-secondary-1)",
+        2: "var(--brand-secondary-2)",
+        3: "var(--brand-secondary-3)",
+        4: "var(--brand-secondary-4)",
+        5: "var(--brand-secondary-5)",
       },
     },
     fontFamily: {
       sans: [
-        'Roboto',
-        'Nunito Sans',
-        '-apple-system',
-        'BlinkMacSystemFont',
+        "Roboto",
+        "Nunito Sans",
+        "-apple-system",
+        "BlinkMacSystemFont",
         '"Segoe UI"',
         '"Helvetica Neue"',
-        'Arial',
+        "Arial",
         '"Noto Sans"',
-        'sans-serif',
+        "sans-serif",
         '"Apple Color Emoji"',
         '"Segoe UI Emoji"',
         '"Segoe UI Symbol"',
         '"Noto Color Emoji"',
       ],
-      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      serif: ["Georgia", "Cambria", '"Times New Roman"', "Times", "serif"],
       mono: [
-        'Menlo',
-        'Monaco',
-        'Consolas',
+        "Menlo",
+        "Monaco",
+        "Consolas",
         '"Liberation Mono"',
         '"Courier New"',
-        'monospace',
+        "monospace",
       ],
     },
   },
@@ -69,6 +72,5 @@ module.exports = {
   },
   plugins: [
     // Some useful comment
-  ]
-}
-
+  ],
+};
