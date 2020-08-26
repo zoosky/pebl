a887413050caba11a9288c6b37dcbe8390ea2a8b
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-25 21:11:48 -0400

    Add tailwindUI

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,76 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
   },
   theme: {
     extend: {
-      spacing: {
-        80: "20rem",
-        108: "27rem",
+      colors: {
+        brand: {
+          light: "#88c0d0",
+          default: "#81a1c1",
+          dark: "#5e81ac",
+        },
       },
-      borderWidth: {
-        14: "14px",
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
-    },
-    container: {
-      padding: "1rem",
-    },
-    colors: {
-      background: {
-        1: "var(--bg-brand-1)",
-        2: "var(--bg-brand-2)",
-        3: "var(--bg-brand-3)",
-      },
-      copy: {
-        base: "var(--text-brand-1)",
-        accent: "var(--text-brand-2)",
-        mute: "var(--text-brand-3)",
-      },
-      primary: {
-        1: "var(--brand-primary-1)",
-        2: "var(--brand-primary-2)",
-        3: "var(--brand-primary-3)",
-        4: "var(--brand-primary-4)",
-      },
-      secondary: {
-        1: "var(--brand-secondary-1)",
-        2: "var(--brand-secondary-2)",
-        3: "var(--brand-secondary-3)",
-        4: "var(--brand-secondary-4)",
-        5: "var(--brand-secondary-5)",
-      },
-    },
-    fontFamily: {
-      sans: [
-        "Roboto",
-        "Nunito Sans",
-        "-apple-system",
-        "BlinkMacSystemFont",
-        '"Segoe UI"',
-        '"Helvetica Neue"',
-        "Arial",
-        '"Noto Sans"',
-        "sans-serif",
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ],
-      serif: ["Georgia", "Cambria", '"Times New Roman"', "Times", "serif"],
-      mono: [
-        "Menlo",
-        "Monaco",
-        "Consolas",
-        '"Liberation Mono"',
-        '"Courier New"',
-        "monospace",
-      ],
     },
   },
-  variants: {
-    // Some useful comment
-  },
-  plugins: [
-    // Some useful comment
-  ],
+  variants: {},
+  plugins: [require("@tailwindcss/ui")],
 };
