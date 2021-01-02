780e933f94afbe71dd8557a5a8f83a9b3f449e78
Tom Konidas <tomkonidas@protonmail.ch>
2021-01-02 12:30:20 -0500

    Bump hex deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pebl.MixProject do
     [
       app: :pebl,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -33,8 +33,8 @@ defmodule Pebl.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.11"},
-      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.5.7"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
